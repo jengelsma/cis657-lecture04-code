635320a74a436ced2fe39ac2f5fa4183387a227e
Jonathan Engelsma <jengelsma@gmail.com>
2021-05-15 17:21:18 -0400

    Finished Part G

--- components/Greeter.js
+++ components/Greeter.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { Button } from 'react-native-elements';
-import React from 'react';
 
 const Greeter = ({greeting, buttonTitle}) => {
+    const [name, setName] = useState('')
+    const [personalGreeting, setPersonalGreeting] = useState(greeting)
+    console.log(name);
     return (
       <View style={styles.container}>
-          <Text> {greeting} </Text>
-          <TextInput placeholder='Enter your name' />
-          <Button title={buttonTitle} />
+          <Text> {personalGreeting} </Text>
+          <TextInput 
+            placeholder='Enter your name'
+            onChangeText={setName}
+            value={name} />
+          <Button 
+            title={buttonTitle}
+            onPress={() => {
+                setPersonalGreeting(`${greeting} to you ${name}`);
+            }} />
       </View>
     );
 
@@ -17,7 +27,6 @@ const Greeter = ({greeting, buttonTitle}) => {
 const styles = StyleSheet.create({
     container: {
         margin: 20, 
-
     }
 });
 
